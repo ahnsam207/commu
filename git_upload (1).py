7a3f0164cbbf67535bc3952da3e62f09c8a983d6
ahnsam207 <2ok25@daum.net>
2025-02-19 10:39:52 +0900

    Update git_upload.py

--- git_upload (1).py
+++ git_upload (1).py
@@ -3,12 +3,12 @@ import requests
 import base64
 
 # GitHub 정보
-GITHUB_REPO = "ahnsam207/res_self"
+GITHUB_REPO = "ahnsam207/res_commu"
 GITHUB_TOKEN =  st.secrets["git_token"]   # 개인 액세스 토큰 입력
 BRANCH = "main"  # 사용할 브랜치
 
 
-st.title("KB 자율장학 계획서 제출")
+st.title("KB 교원학습공동체 계획서 제출")
 # 파일 업로드
 uploaded_file = st.file_uploader(" 계획서 파일을 업로드하세요")
 
